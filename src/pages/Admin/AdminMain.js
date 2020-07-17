2921f1da596ac0bd04a8272030170c3ab54043c5
srnazng <srnazng@gmail.com>
2020-07-16 21:35:34 -0400

    initial

---
+++ src/pages/Admin/AdminMain.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import { request } from "../../util";
+import { makeStyles } from '@material-ui/core/styles';
+import { AdminHome } from './Home';
+import { AllTickets } from './Tickets';
+
+import {
+    Route,
+    BrowserRouter as Router,
+    Switch,
+    Redirect,
+} from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+}));
+
+const AdminMain = () => {
+    const [tickets, setTickets] = useState([]);
+    const [page, setPage] = useState(0);
+    const classes = useStyles();
+
+    useEffect(() => {
+        const update = async () => {
+            setTickets(await request({ path: "/tickets/" }));
+        };
+
+        const interval = setInterval(update, 3000);
+        update();
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
+
+    return (
+        <div>
+            <Switch>
+                <Route path="/" component={AdminHome} />
+                <Route path="/admin/home" component={AdminHome} />
+                <Route path="/admin/alltickets" component={AllTickets} />
+            </Switch>
+        </div>
+    );
+};
+
+export { AdminMain };
---
+++ src/pages/Admin/Tickets.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import { TicketContainer } from "../../components/TicketContainer";
+import DashboardContainer from "../../components/DashboardContainer";
+import { request } from "../../util";
+import { Grid } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import MenuListComposition from '../../components/AdminView/Menu';
+import { AdminMain } from "./AdminMain";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+}));
+
+const AllTickets = () => {
+    const [tickets, setTickets] = useState([]);
+    const classes = useStyles();
+
+    useEffect(() => {
+        const update = async () => {
+            setTickets(await request({ path: "/tickets/" }));
+        };
+
+        const interval = setInterval(update, 3000);
+        update();
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
+
+    console.log("Ticket Page")
+
+    return (
+        <DashboardContainer>
+            <div className={classes.root}>
+                <Grid container spacing={2}>
+                    <Grid item xs={3}>
+                        <MenuListComposition />
+                    </Grid>
+                    <Grid item xs={8}>
+                        <TicketContainer tickets={tickets} />
+                    </Grid>
+                </Grid>
+            </div>
+        </DashboardContainer>
+    );
+};
+
+export { AllTickets };
