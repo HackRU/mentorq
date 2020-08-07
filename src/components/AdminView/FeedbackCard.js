a5a76f3ca1af08ea03b301bd19201f0a70262d57
srnazng <srnazng@gmail.com>
2020-08-06 21:35:32 -0400

    rating display

--- src/components/AdminView/FeedbackCard.js
+++ src/components/AdminView/FeedbackCard.js
@@ -1,37 +1,35 @@
 import React, { useState, useEffect } from "react";
 import { request } from "../.././util";
-import { logoutUser } from "../../actions";
-import { useDispatch, useSelector } from "react-redux";
 import {
     CardContent,
     Card,
     FormLabel as Label,
     Grid,
     Typography,
-    Button,
-    ButtonGroup,
-    TextField,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogContentText,
-    DialogTitle,
 } from "@material-ui/core";
 import Rating from '@material-ui/lab/Rating';
+import { makeStyles } from '@material-ui/core/styles';
 
-
+const useStyles = makeStyles((theme) => ({
+    root: {
+    },
+    rating: {
+        position: "absolute",
+        top: "5px",
+        right: "5px",
+    }
+}));
 
 const FeedbackCard = ({
     feedback: { id, comments, rating },
 }) => {
     const [ticket, setTicket] = useState([]);
+    const classes = useStyles();
 
     useEffect(() => {
-
         const update = async () => {
             setTicket(await request({ path: `/tickets/${id}` }));
         };
-
         const interval = setInterval(update, 3000);
         update();
         return () => {
@@ -42,18 +40,23 @@ const FeedbackCard = ({
 
 
     return (
-        <Card>
+        <Card className={classes.root} >
             <CardContent>
                 <Grid container spacing={1}>
-                    <Grid item xs={12}>
-                        <Typography variant="h5" gutterBottom>
+                    <Grid item xs={9}>
+                        <Typography variant="h5" gutterBottom >
                             Feedback for {ticket.title}
                         </Typography>
                     </Grid>
-                    <Grid item xs={4}>
-                        <Label>Rating</Label>
+                    <Grid item xs={1} justify="space-between">
+                        <Typography variant="h5" gutterBottom >
+                            <Rating name="read-only" value={rating} readOnly />
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Label>Comments</Label>
                         <Typography variant="body1" gutterBottom>
-                            {rating} stars
+                            {comments}
                         </Typography>
                     </Grid>
                     <Grid item xs={4}>
@@ -66,12 +69,6 @@ const FeedbackCard = ({
                         <Label>Mentor</Label>
                         <Typography variant="body1" gutterBottom>
                             {ticket.mentor}
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Label>Comments</Label>
-                        <Typography variant="body1" gutterBottom>
-                            {comments}
                         </Typography>
                     </Grid>
                 </Grid>
