6c7ca96327727b4468368bd289a48e11f818cb05
Lucent Fong <lucent.fong@gmail.com>
2020-08-27 16:44:16 -0400

    small-fix

--- src/actions/login.js
+++ src/actions/login.js
@@ -25,14 +25,13 @@ const recievedLoginUser = ({ ...values }) => ({
 const failedLoginUser = () => ({ type: FAILED_LOGIN });
 
 function parseJwt(token){
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-    console.log(jsonPayload)
-    return JSON.parse(jsonPayload);
-};
+  var base64Url = token.split(".")[1];
+  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  var jsonPayload = decodeURIComponent(atob(base64).split("").map(function (c) {
+      return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(""));
+  return JSON.parse(jsonPayload);
+}
 
 const loginUser = ({ email, password }) => {
   return async (dispatch) => {
