d87c228f2f2cf16a26de0920007ad48426224fbb
kayla-kam <kayla.kam.1006@gmail.com>
2021-01-14 13:34:52 -0500

    time slot fixed

--- src/components/Ticket/index.js
+++ src/components/Ticket/index.js
@@ -97,8 +97,36 @@ const Ticket = ({
 
   const getTimeDifference = (timeA, timeB) => {
     const timeInMilliseconds = timeA.valueOf() - timeB.valueOf();
-    const timeInHours = Math.round((timeInMilliseconds / 1000) / 60 / 60);
-    return `${timeInHours} hour${timeInHours > 1 ? "s" : ""}`
+    const timeInSecs = Math.round(timeInMilliseconds/1000);
+    let finaltime = "";
+    let timeInHours = "";
+    let timeInMinutes = "";
+    /*const timeInMins = (timeInSecs / 60);
+    const timeInHours = Math.round((timeInMins / 60));
+    return `${timeInHours} hour${timeInHours > 1 ? "s" : ""}`*/
+    if(timeInSecs > 3600){
+      const timeInHours = Math.round((timeInSecs / 3600));
+      finaltime = timeInHours + " hours ";
+      if((timeInSecs % 60) > 0){
+        timeInMinutes = Math.round(timeInSecs%60);
+        finaltime = finaltime + timeInMinutes + " minutes ";
+      }
+      return finaltime; 
+    }
+    else if(timeInSecs > 60){
+      timeInMinutes = Math.round(timeInSecs / 60);
+      finaltime = timeInMinutes + " minutes ";
+      return finaltime; 
+    }
+    else if(timeInSecs > 1){
+      finaltime = timeInSecs + " seconds ";
+      return finaltime; 
+    }
+    else{
+      timeInSecs = Math.round(timeInSecs);
+      finaltime = timeInSecs;
+      return finaltime;
+    }
   }
 
   useEffect(() => {
